6cde3badc28eb90b863d2ae834c0186b9ebc23a9
Debjeet Banerjee <136410764+mintRaven-05@users.noreply.github.com>
2024-08-11 07:11:48 +0530

    imports and META-DATA

---
+++ mintorrent.py
@@ -0,0 +1,17 @@
+import sys
+import subprocess
+from os import path
+from tabulate import tabulate
+from utils.magnet import Magnet
+from utils.download import Downloader
+from utils.torrent import Torrent
+
+# ---------------------------------------------------------------------------------------------------
+_TOOL_ = "MinTorrent"
+_VERSION_ = "0.0.1"
+_AUTHOR_ = "Debjeet Banerjee (mintRaven)"
+_LICENSE_ = "GNU General Public License Version-3"
+_COPYRIGHT_ = "Copyright (C) 2024, Debjeet Banerjee"
+_DATE_ = "10th August, 2024"
+_WEBSITE_ = "https://github.com/mintRaven-05"
+# ---------------------------------------------------------------------------------------------------
