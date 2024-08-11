2baf687acf8a709785d679d7f7dbbde7c2c76027
Debjeet Banerjee <136410764+mintRaven-05@users.noreply.github.com>
2024-08-11 07:07:08 +0530

    remove system links and locals

--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,11 @@ echo "[UNINSTALL] Removing files"
 
 echo "[+] removing $HOME/.mintorrent/"
 rm -rf $HOME/.mintorrent
+sleep 0.6
+echo "[+] removing $HOME/.local/mintorrent/"
+rm -rf $HOME/.local/mintorrent
+sleep 0.2
+echo "[+] removing system links from /usr/bin"
+sleep 0.4
+echo "[+] removing /usr/bin/mintorrent"
+sudo rm -rf /usr/bin/mintorrent
