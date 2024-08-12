ca1cf9da571f8ba85b54129a6b3081eec5e84e38
Debjeet Banerjee <136410764+mintRaven-05@users.noreply.github.com>
2024-08-12 06:41:15 +0530

    generate download stub (PYC-TOX) for magnets

--- utils/download.py
+++ utils/download.py
@@ -20,4 +20,44 @@ class Downloader:
         p = math.pow(1024, i)
         s = round(size_bytes / p, 2)
         return "%s %s" % (s, size_tuple[i])
-    
+    def get_path(self):
+        try:
+            self.save_path = input("\033[32;1m[+] enter the save path: \033[37m")
+            if self.save_path.split() == []:
+                print("\033[32;1m[+] Selecting default save path: \033[37m~/Downloads/")
+                self.save_path = "~/Downloads/"
+                self.expanded_path = path.expanduser(self.save_path)
+            else:
+                self.expanded_path = path.expanduser(self.save_path)
+                return self.expanded_path
+
+        except KeyboardInterrupt:
+            print("\033[31;1m\n[~] exiting . . .\033[37m")
+            sys.exit(0)
+
+        except ValueError:
+            print(
+                "\033[33;1m[!] error detecting save path, going with defaults (path: ~/Downloads)\033[37m"
+            )
+            self.save_path = "~/Downloads/"
+            self.expanded_path = path.expanduser(self.save_path)
+            return self.expanded_path
+
+        except TypeError:
+            print(
+                "\033[33;1m[!] error detecting save path, going with defaults (path: ~/Downloads)\033[37m"
+            )
+            self.save_path = "~/Downloads/"
+            self.expanded_path = path.expanduser(self.save_path)
+            return self.expanded_path
+    def gen_download_stub(self):
+        try:
+            self.info.save_path = self.expanded_path
+            session_card = self.session.add_torrent(self.info)
+            download_stub = session_card.status()
+            return download_stub
+        except Exception:
+            print(
+                "\033[33;1m[!] error generating download stub, could not add magnet to session\033[37m"
+            )
+
